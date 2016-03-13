7f9825c7b6ba9daf000fa6d02a771d951f5c461d
Jaymin-Vijay-Patel <Jaymin.Vijay.Patel@gmail.com>
2016-03-13 13:43:36 -0400

    Updated Lab5 scripts

--- Lab5.m
+++ Lab5.m
@@ -29,7 +29,7 @@ pixelclock = 7; %Set the speed of pixel readout.
 %     end
         
 %Part 2. Transmissive Color Scene.
-    T = cell(1,7);
+%     T = cell(1,7);
     exposure = 100;
     [~,T{1}] = capture_frames(C,{'pixelclock',pixelclock},{'aoi',aoi},{'exposure',exposure},{'frames',frames});
     [~,T{2}] = capture_frames(C,{'pixelclock',pixelclock},{'aoi',aoi},{'exposure',exposure},{'frames',frames});
@@ -41,19 +41,53 @@ pixelclock = 7; %Set the speed of pixel readout.
     [~,T{7}] = capture_frames(C,{'pixelclock',pixelclock},{'aoi',aoi},{'exposure',exposure},{'frames',frames});
     
 %Part 3. Analyze Data.
-    e = 0.02;
-    D = inv(diag(db2.*ds.*dl+e));
-    A = inv(L'*L)*L'*D*F(1:5,:)'*inv(F(1:5,:)*F(1:5,:)'); %Determine matrix A.
-    Rv = zeros(5,1280*1024);
-    for i = 1:size(Rv,1)
-        Rv(i,:) = R2{i}(:)';
-    end
-    Rz = A*Rv;
-    Rrgb = zeros(1280,1024,3); 
-    for i = 1:size(Rrgb,3)
-        Rrgb(:,:,i) = reshape(Rz(i,:)',[1280 1024]);
-    end
-    
-    
+    %Form Color Images.
+        %Determine Matrix A.
+            e = 0.02;
+            D = inv(diag(db1.*ds.*dl+e));
+            A = inv(L'*L)*L'*D*F(1:5,:)'*inv(F(1:5,:)*F(1:5,:)');
+        %Reflective.
+            Rv = zeros(5,1280*1024);
+            for i = 1:size(Rv,1)
+                Rv(i,:) = R2{i}(:)';
+            end
+            Rz = A*Rv;
+            Rrgb = zeros(1280,1024,3); 
+            for i = 1:size(Rrgb,3)
+                Rrgb(:,:,i) = reshape(Rz(i,:)',[1280 1024]);
+            end
+            Rrgb = Rrgb/max(Rrgb(:));
+        %Transmissive.
+            Tv = zeros(5,1280*1024);
+            for i = 1:size(Tv,1)
+                Tv(i,:) = T{i}(:)';
+            end
+            Tz = A*Tv;
+            Trgb = zeros(1280,1024,3); 
+            for i = 1:size(Trgb,3)
+                Trgb(:,:,i) = reshape(Tz(i,:)',[1280 1024]);
+            end
+            Trgb = Trgb/max(Trgb(:));
+    %Principal Component Analysis
+        %Reflective.
+            [Ur,Sr,Vr] = svd(Rv,'econ');
+            Vnb = zeros(1280,1024,5);
+            for i = 1:size(Vr,2)
+                Vnb(:,:,i) = reshape(Vr(:,i),[1280 1024]);
+            end
+        %Transmissive.
+            [Ut,St,Vt] = svd(Tv,'econ');
+            Tnb = zeros(1280,1024,5);
+            for i = 1:size(Tr,2)
+                Tnb(:,:,i) = reshape(Tr(:,i),[1280 1024]);
+            end
+    %K-Means Clustering
+        %Reflective
+            Ridx = kmeans(Rv',12,'MaxIter',1000);
+            Rk = reshape(Ridx,[1280 1024]);
+            imagesc(Rk'); colorbar;
+        %Transmissive
+            
+        
 % delete(C);
 % clear C;
