2019f495b2fdc6fc2943df04ace9249c6ef2b03b
Jaymin-Vijay-Patel <Jaymin.Vijay.Patel@gmail.com>
2016-04-15 10:29:27 -0400

    Created files for everyone to work on Final Project

---
+++ Functions/motion_operator_2d.m
@@ -0,0 +1,9 @@
+function Fimg = motion_operator_2d(img,position)
+%MOTION_OPERATOR_2D Interpolate images based on position relative to first image.
+
+    
+    for i = 1:size(img,3)
+        
+    end
+
+end
