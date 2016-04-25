4bd4add4101ce3561b366604a75d23c9f5793988
Nathan Crookston <ncrooks1@jhu.edu>
2016-04-25 14:05:16 -0400

    Fix bug in regularization

--- Functions/superresolution.m
+++ Functions/superresolution.m
@@ -6,10 +6,10 @@ X_dim = [size(Yk,1) size(Yk,2)].*factor;
 
 Xn_hat = zeros(X_dim);
 [xt,yt] = meshgrid(0:size(Yk,3)-1,0:size(Yk,4)-1);
-%xt = xt * .5;
-%yt = yt * .5;
+xt = xt * .5;
+yt = yt * .5;
 
-do_clear_memory = 1;
+do_clear_memory = 0;
 for N = 1:steps
     Yk_hat = fproj(Xn_hat,Y_dim, xt, yt);
     sign_Yk_hat = sign(Yk_hat-Yk);
@@ -28,10 +28,10 @@ for N = 1:steps
     for l = 0:P
         for m = -l:P
             Xn_reg_sign = sign(Xn_hat - imtranslate(Xn_hat, [l m]));
-            Xn_reg = Xn_reg + (Xn_reg_sign - imtranslate(Xn_hat, [-l -m])) * alpha^(abs(m) + abs(l));
+            Xn_reg = Xn_reg + (Xn_reg_sign - imtranslate(Xn_reg_sign, [-l -m])) * alpha^(abs(m) + abs(l));
         end
     end
-    Xn_hat = Xn_hat-beta*(Xn_hat_error + lambda * Xn_reg);
+    Xn_hat = Xn_hat - beta*(Xn_hat_error + lambda * Xn_reg);
     if do_clear_memory > 0
         clear('Xn_hat_error');
     end
